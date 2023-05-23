d23ef213af78ffee8864cb39db3102dc31baa5bb
lvovgit <romka.lvov@gmail.com>
2023-05-24 00:29:08 +0300

    fix problems with images

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,9 +1,15 @@
+from django.conf.urls.static import static
 from django.urls import path
 from catalog.views import home, info
 from catalog.apps import CatalogConfig
+from config import settings
+
 app_name = CatalogConfig.name
 urlpatterns = [
 
-    path('', home, name='home'),
-    path('contacts/', info, name='contacts'),
-]
+                  path('', home, name='home'),
+                  path('contacts/', info, name='contacts'),
+              ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# if settings.DEBUG:
+#     urlpatterns += static(settings.MEDIA_URL,
+#                           document_root=settings.MEDIA_ROOT)
--- catalog/views.py
+++ catalog/views.py
@@ -1,12 +1,18 @@
+import os
+
 from django.shortcuts import render
 
 from catalog.models import Product
+from config import settings
 
 
 def home(request):
+    path = settings.MEDIA_ROOT
+    img_list = os.listdir(path + '/images')
     context = {
-        'product_list': Product.objects.all()[:3],
-        'title': 'Список продуктов'
+        'product_list': Product.objects.all()[:4],
+        'title': 'Список продуктов',
+        'images': img_list
     }
     return render(request, 'catalog/home.html', context)
 
